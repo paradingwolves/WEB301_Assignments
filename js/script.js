3e37410a88dd15ad8ca92aa4b1fd40882042b92a
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2022-12-01 14:29:09 -0500

    AJAX & Accordion
    Added an accordion to store some of my content. To clean up the html I am now using AJAX to retrieve content

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+    $('#headingOne').on('click', function(){
+        var xhr = new XMLHttpRequest;
+        xhr.open('GET','gameMD.html',true);
+        xhr.onload = function() {
+            if(this.status=== 200)
+            {
+                let el = document.getElementById("gameMD2");
+                el.innerHTML=xhr.responseText;
+                let $content = $("#headingOne");
+                $content.css ({
+                    'display': "block"
+                });
+            }
+        }   
+        xhr.send();
+    });
+
+    $('#headingTwo').on('click', function(){
+        var xhr = new XMLHttpRequest;
+        xhr.open('GET','gameCycle.html',true);
+        xhr.onload = function() {
+            if(this.status=== 200)  {
+                let el = document.getElementById("gameCycle2");
+                el.innerHTML=xhr.responseText;
+                let $content = $("#headingTwo");
+                $content.css ({
+                'display': "block"
+                });
+            }
+        }   
+        xhr.send();
+    });
+    $('#headingThree').on('click', function(){
+        var xhr = new XMLHttpRequest;
+        xhr.open('GET','liquidationCenter.html',true);
+        xhr.onload = function() {
+            if(this.status=== 200)  {
+                let el = document.getElementById("liquidationCenter2");
+                el.innerHTML=xhr.responseText;
+                let $content = $("#headingThree");
+                $content.css ({
+                'display': "block"
+                });
+            }
+        }   
+        xhr.send();
+    });
+});
+
